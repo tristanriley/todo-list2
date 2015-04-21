e6de2ad50ea858c6922962d692bd080110c53396
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-21 10:52:58 -0700

    milestone 1 and 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
