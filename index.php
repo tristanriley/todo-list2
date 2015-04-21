d86840f1d0665f209f118db01def741294adfc45
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-21 11:29:46 -0700

    video 6

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Simple To-Do List</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-<body>
-	<div class="wrap">
-	
-	</div>
-</body>
+	<head>
+		<title>Todo-List</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<div class="wrap"></div>
+		<div class="task-list">
+			<ul>
+				<?php require("inludes/connect.php"); ?>
+			</ul>
+		</div>
+		<form class="add-new-task" autocomplete = "off">
+			<input type="text" name="new-task" placeholder="Add new item..."/> 
+		</form>
+	</body>
 </html>
