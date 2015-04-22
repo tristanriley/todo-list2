47db55f3ae7674f6149d6cafced943c200037278
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-22 11:09:35 -0700

    milestone 12

--- includes/add-task.php
+++ includes/add-task.php
@@ -11,4 +11,12 @@
 	$mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
 	//displays the saved data
 	$query = "SELECT * FROM tasks WHERE task = '$task' and date = 'date' and time = 'time'";
+	if ($result = $mysqli->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+			$task_id = $row['id'];
+			$task_name = $row['task'];
+		}
+	}
+	$mysqli->close();
+	echo '<li><span>' . $task_name . '</span><img id = "' . task_id . '" class = "delete-button" width = "10px" src = "images/close.svg" /></li>';
 ?>
