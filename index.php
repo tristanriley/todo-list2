4ba9073f99d63d1eae0e844b7e0964d5b5b3d3ca
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-22 10:59:18 -0700

    miles stone  9

--- index.php
+++ index.php
@@ -18,4 +18,23 @@
 			<input type="text" name="new-task" placeholder="Add new item..."/> 
 		</form>
 	</body>
+	<scripts src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script>
+		//calls add_task function 
+		add_task();
+		//creates add_task function
+		function add_task(){
+			$('.add-new-task').submit(function(){
+				var new_task = $('.add-new-task input[name=new-task').val();
+				if (new_task != '') {
+					$.post('includes/add-task.php', { task: new_task}, function(data){
+						$(('add-new-task input[name=new-task]').val();
+							$(data).appendTo('task-list ul').hide().fadeIn();
+						)
+					});
+				}
+				return false;
+			});
+		}
+	</script>
 </html>
