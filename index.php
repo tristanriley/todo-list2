08457aedf4d634b9904ca532b5b90aa4cd656b90
Tristan Whitaker <tristanrileyw@gmail.com>
2015-04-22 10:53:53 -0700

    milestone 8

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 <html>
 	<head>
 		<title>Todo-List</title>
+		<!-- links main css file -->
 		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<link rel="stylesheet" type="text/css" href="css/normalize.css">
-		<link rel="stylesheet" type="text/css" href="css/reset.css">
 	</head>
 	<body>
 		<div class="wrap"></div>
 		<div class="task-list">
 			<ul>
-				<?php require("inludes/connect.php"); ?>
+				<!-- inserts connect.php file into this div -->
+				<?php require("includes/connect.php"); ?>
 			</ul>
 		</div>
+		<!-- inserts a box for inserting text -->
 		<form class="add-new-task" autocomplete = "off">
 			<input type="text" name="new-task" placeholder="Add new item..."/> 
 		</form>
